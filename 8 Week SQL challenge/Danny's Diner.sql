8b5517ba7398e29e156746a81684cf090bef8da1
Akash Kumar <118870405+akashsinghsharma@users.noreply.github.com>
2023-10-01 13:46:11 +0530

    Update Danny's Diner.sql

--- 8 Week SQL challenge/Danny's Diner.sql
+++ 8 Week SQL challenge/Danny's Diner.sql
@@ -41,3 +41,58 @@ C	2
 
 -- 3. What was the first item from the menu purchased by each customer?
 
+-- Here using temp tables and windows function.
+
+WITH order_table AS (
+select
+	s.customer_id,
+	s.order_date,
+	s.product_id,
+	DENSE_RANK() OVER(PARTITION BY s.customer_id ORDER BY s.order_date) AS 'order_rank',
+	m.product_name
+FROM sales s
+JOIN menu m
+ON s.product_id = m.product_id
+)
+
+SELECT 
+	customer_id,
+	product_name
+FROM order_table
+WHERE order_rank = 1
+GROUP BY customer_id, product_name;
+
+/*
+Output:
+A	curry
+A	sushi
+B	curry
+C	ramen
+*/
+
+-- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
+
+WITH freq_list AS (
+SELECT 
+	s.product_id,
+	m.product_name,
+	COUNT(s.product_id) AS times_item_purchases
+FROM sales s
+JOIN menu m
+ON s.product_id = m.product_id
+GROUP BY s.product_id, m.product_name
+)
+
+SELECT
+	product_name,
+	times_item_purchases
+FROM freq_list
+WHERE  times_item_purchases = (SELECT MAX(times_item_purchases) FROM freq_list);
+
+/*
+Output:
+ramen	8
+*/
+
+-- This question could have been solved without using SQL CTE. Solving with that method:
+
