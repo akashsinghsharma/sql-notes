4dc9c002bdf2e21282c5b3a3b058361ddf696f85
Akash Kumar <118870405+akashsinghsharma@users.noreply.github.com>
2023-10-03 17:12:52 +0530

    Update Danny's Diner.sql- Day 2

--- 8 Week SQL challenge/Danny's Diner.sql
+++ 8 Week SQL challenge/Danny's Diner.sql
@@ -73,21 +73,29 @@ C	ramen
 -- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
 
 WITH freq_list AS (
+  SELECT 
+    s.product_id, 
+    m.product_name, 
+    COUNT(s.product_id) AS times_item_purchases 
+  FROM 
+    sales s 
+    JOIN menu m ON s.product_id = m.product_id 
+  GROUP BY 
+    s.product_id, 
+    m.product_name
+) 
 SELECT 
-	s.product_id,
-	m.product_name,
-	COUNT(s.product_id) AS times_item_purchases
-FROM sales s
-JOIN menu m
-ON s.product_id = m.product_id
-GROUP BY s.product_id, m.product_name
-)
-
-SELECT
-	product_name,
-	times_item_purchases
-FROM freq_list
-WHERE  times_item_purchases = (SELECT MAX(times_item_purchases) FROM freq_list);
+  product_name, 
+  times_item_purchases 
+FROM 
+  freq_list 
+WHERE 
+  times_item_purchases = (
+    SELECT 
+      MAX(times_item_purchases) 
+    FROM 
+      freq_list
+  );
 
 /*
 Output:
@@ -95,4 +103,87 @@ ramen	8
 */
 
 -- This question could have been solved without using SQL CTE. Solving with that method:
+
+SELECT 
+  TOP 1 m.product_name, 
+  COUNT(s.product_id) AS 'freq' 
+FROM 
+  sales s 
+  JOIN menu m ON s.product_id = m.product_id 
+GROUP BY 
+  s.product_id, 
+  m.product_name 
+ORDER BY 
+  freq DESC;
+
+-- Note that in MYSQL, TOP function is not available so use LIMIT while using MYSQL.
+
+-- 5. Which item was the most popular for each customer?
+
+WITH order_count AS (
+  SELECT 
+    customer_id, 
+    product_id, 
+    COUNT(product_id) AS 'order_count', 
+    DENSE_RANK() OVER(
+      PARTITION BY customer_id 
+      ORDER BY 
+        COUNT(product_id) DESC
+    ) AS 'rank' 
+  FROM 
+    sales 
+  GROUP BY 
+    customer_id, 
+    product_id
+) 
+SELECT 
+  o.customer_id, 
+  m.product_name, 
+  o.order_count 
+FROM 
+  order_count AS o 
+  JOIN menu AS m ON o.product_id = m.product_id 
+WHERE 
+  rank = 1;
+
+/*
+Output:
+A	ramen	3
+B	sushi	2
+B	curry	2
+B	ramen	2
+C	ramen	3
+*/
+
+-- 6. Which item was purchased first by the customer after they became a member?
+
+-- To solve this que, I am making an assumption that we are fetching records of all the items ordered after they became member, not just one item.
+-- To fetch record of one item only, we can use row_number() in the windows function, I used dense_rank() instead.
+
+WITH order_rank AS (
+SELECT
+	s.customer_id,
+	s.order_date,
+	s.product_id,
+	DENSE_RANK() OVER(PARTITION BY s.customer_id ORDER BY order_date) AS 'order_rank'
+FROM 
+	sales s
+	JOIN members mem ON mem.customer_id = s.customer_id
+	AND s.order_date > mem.join_date
+)
+
+SELECT
+	o.customer_id,
+	o.order_date,
+	m.product_name
+FROM order_rank o
+JOIN menu m
+ON m.product_id = o.product_id
+WHERE order_rank = 1;
+
+/*
+Output:
+A	2021-01-10	ramen
+B	2021-01-11	sushi
+*/
 
